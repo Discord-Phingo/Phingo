123fca44a677e840f0bdab324e991a027f83d562
NateKanyok <nkanyok@kent.edu>
2020-08-13 14:45:13 -0400

    add autocomplete component

--- alpha/src/components/Square.jsx
+++ alpha/src/components/Square.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import Box from "@material-ui/core/Box";
-import SongPick from "./TextField";
+// import SongPick from "./TextField";
+import AutoCompleteText from "./AutoCompleteText";
+import songs from "./songs";
 
 export default class Square extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
   render() {
     return (
       <Box>
-        <SongPick />
+        {/* <SongPick /> */}
+        <AutoCompleteText items={songs} />
       </Box>
     );
   }
---
+++ alpha/src/components/songs.js
@@ -0,0 +1,1025 @@
+export default `1999
+20-20 Vision
+2001
+35 Minute Jam
+46 Days
+5:15
+50 Ways to Leave Your Lover
+555
+99 Problems
+A Apolitical Blues
+A Day in the Life
+A Life Beyond The Dream
+A Song I Heard the Ocean Sing
+About to Run
+AC/DC Bag
+Access Me
+Acoustic Army
+ADITL
+After Midnight
+Aftermath
+Ain't Love Funny
+Alaska
+Albert
+Albuquerque
+All Along the Watchtower
+All Blues
+All Down the Line
+All Of Me
+All of These Dreams
+All That You Dream
+All the Pain Through the Years
+All Things Reconsidered
+Also Sprach Zarathustra
+Alumni Blues
+Amazing Grace
+Ambient Jam
+American Woman
+Amidst the Peals of Laughter
+Amoreena
+Anarchy
+And It Stoned Me
+And So To Bed
+Andy's Chest
+Another One Bites the Dust
+Antelope
+Any Colour You Like
+Anything But Me
+Arc
+Architect
+Army of One
+Arrival
+Art Jam
+ASIHTOS
+Ass Handed
+ATR
+Auld Lang Syne
+Autumn Leaves
+Avenu Malcanu
+Avenu Malkenu
+Axilla
+Axilla (Part II)
+Baby Elephant Walk
+Baby Lemonade
+Babylon Baby
+Babylon is Burning
+Back at the Chicken Shack
+Back in the U.S.S.R.
+Back on the Train
+Back Porch Boogie Blues
+Backwards Down the Number Line
+Bake and Boil
+Band/Crew Football Theme Song
+Basin Street Blues
+Bathtub Gin
+BBFCFM
+Beaumont Rag
+Beauty of a Broken Heart
+Beauty of My Dreams
+Been Caught Stealing
+Bell Boy
+Beneath a Sea of Stars Part 1
+Bertha
+Big Alligator
+Big Ball Jam
+Big Balls
+Big Black Furry Creature from Mars
+Big Leg Emma
+Big Pimpin'
+Bike
+Bill Bailey, Won't You Please Come Home?
+Billie Jean
+Billy Breathes
+Birds of a Feather
+Birthday
+Birthday Boys
+Bitchin' Again
+Bittersweet Motel
+Black and Tan Fantasy
+Black-Eyed Katy
+Blackberry Blossom
+Blackbird
+Blaze On
+Bliss
+Blister in the Sun
+Blue Bayou
+Blue Bossa
+Blue Monk
+Blue Moon
+Blue Over Yellow
+Blue Skies
+Blue Sky
+Bobby Jean
+Bohemian Rhapsody
+Bold As Love
+Boogie On Reggae Woman
+Born to Run
+Born Under Punches (The Heat Goes On)
+BOTT
+Bouncing Around the Room
+Brain Damage
+Breath and Burning
+Breathe
+Breathe Reprise
+Brian and Robert
+Bring It On Home
+Brother
+Brown Eyed Girl
+Bubble Wrap
+Buffalo Bill
+Bug
+Bundle of Joy
+Buried Alive
+Burn That Bridge
+Burning Down the House
+Bye Bye Foot
+California Love
+Call It Stormy Monday (But Tuesday Is Just As Bad)
+Camel Walk
+Can't Always Listen
+Can't Come Back
+Can't You Hear Me Knocking
+Cannonball
+Caravan
+Carefree
+Carini
+Carolina
+Carolina in the Morning
+Cars Trucks Buses
+Casino Boogie
+Catapult
+Cathy's Clown
+Cavern
+Cecilia
+Chalk Dust Torture
+Chalk Dust Torture Reprise
+Champagne Supernova
+Character Zero
+Chariots of Fire
+Che Hun Ta Mo
+Chocolate Rain
+Choo Choo Ch' Boogie
+Cinnamon Girl
+Cities
+Clod
+Clone
+Cocaine
+Coconut
+Cold as Ice
+Cold Rain and Snow
+Cold Water
+Colonel Forbin's Ascent
+Come On (Let the Good Times Roll)
+Come On (Part One)
+Come on Baby Let's Go Downtown
+Come Together
+Communication Breakdown
+Contact
+Cool Amber and Mercury
+Cool It Down
+Cool Jerk
+Corinna
+Corona
+Costume Contest
+Couch Lady
+Cracklin' Rosie
+Crazy Sometimes
+Crimes of the Mind
+Crosseyed and Painless
+Crossroads
+Crowd Control
+Cry Baby Cry
+Cryin'
+Cut My Hair
+Dahlia
+Daniel Saw the Stone
+Dave's Energy Guide
+David Bowie
+Day or Night
+Day-O (The Banana Boat Song)
+Dazed and Confused
+Dear Mrs. Reagan
+Dear Prudence
+Death Don't Hurt Very Long
+Dec 1661
+Dem Bones
+Demand
+Den of Iniquity
+Destiny Unbound
+Devotion To a Dream
+Diamond Girl
+Digital Delay Loop Jam
+Dinner and a Movie
+Dirt
+Discern
+Divided Sky
+Dixie Cannonball
+Dixie Chicken
+Doctor Jimmy
+Dog Faced Boy
+Dog Gone Dog
+Dog Log
+Dogs Stole Things
+Doin' My Time
+Don't Bogart That Joint
+Don't Get Me Wrong
+Don't Pass Me By
+Don't Want You No More
+Don't You Want To Go?
+Done Me Wrong
+Donna Lee
+Dooley
+Doomsday
+Down By the River
+Down with Disease
+Dr. Gabel
+Drift While You're Sleeping
+Drifting
+Drive-In Jam
+Driver
+Drowned
+Drums
+Dust in the Wind
+Earache My Eye
+Easy To Slip
+Eclipse
+El Paso
+Eliza
+Emotional Rescue
+End of Session
+Energy
+Esther
+Evening Song
+Everybody's Got Something to Hide Except Me and My Monkey
+Everyday I Have the Blues
+Everything In Its Right Place
+Everything is Hollow
+Everything's Right
+Eyes of the World
+Faht
+Family Picture
+Farmhouse
+Fast Enough for You
+Fat Man in the Bathtub
+Feats Don't Fail Me Now
+Fee
+Feel the Heat
+Fikus
+Final Flight
+Fire
+Fire on the Mountain
+Fire Up the Ganja
+First Tube
+Fish Bass
+Fishman's Gull Poem
+Five Years
+Fixin' to Die
+Flat Fee
+Flatbed Truck Jam
+Flip
+Fluff's Travels
+Fluffhead
+Fly Famous Mockingbird
+Fly Like an Eagle
+Foam
+Foggy Mountain Breakdown
+Fooled Around and Fell in Love
+Fooled by Images
+Foreplay/Long Time
+Four
+Four Strong Winds
+Frankenstein
+Frankie Says
+Free
+Free Bird
+Free Man in Paris
+Freebird
+Freeworld
+Friday
+Friend
+Friends
+Frost
+Fuck Your Face
+Fuckerpants
+Fuego
+Funky (Breakdown)
+Funky Bitch
+Further On Up the Road
+Garden Party
+Gatekeeper
+Gaul Swerves and the Rest is Everything Else
+Get Back on the Train
+Gettin' Jiggy Wit' It
+Ghost
+Ghosts of the Forest
+Ginseng Sullivan
+Girls Girls Girls
+Glass Onion
+Glide
+Glide II
+Gloria
+Gloria
+Glory Days
+Goin' Down Slow
+Gold Soundz
+Golden Age
+Golden Lady
+Golgi Apparatus
+Gone
+Good Times Bad Times
+Got My Mojo Working
+Gotta Jibboo
+Green Onions
+Grind
+GTBT
+Guantanamo Strut
+Guelah Papyrus
+Gumbo
+Guy Forget
+Guyute
+Gypsy Queen
+Ha Ha Ha
+Halfway Home
+Halfway to the Moon
+Hall in Solace
+Halley's Comet
+Hang On to Yourself
+Happiness Is a Warm Gun
+Happy
+Happy Birthday to You
+Harpua
+Harry Hood
+Have Mercy
+He Ent to the Bog
+Head Held High
+Heart and Soul
+Heartache
+Heartbreaker
+Heavy Rotation
+Heavy Things
+Hell's Bells
+Hello My Baby
+Help Me
+Help on the Way
+Helpless
+Helpless Dancer
+Helter Skelter
+Hey You
+High-Heel Sneakers
+Highway to Hell
+Hold to a Dream
+Hold Whatcha Got
+Hold Your Head Up
+Home
+Honey Pie
+Honky Tonk Women
+Hoochie Coochie Man
+Horn
+Houses in Motion
+How High the Moon
+How Many People Are You
+Hurricane
+HYHU
+I Always Wanted It This Way
+I Am Hydrogen
+I Am the Sea
+I Am the Walrus
+I Been Around
+I Been to Georgia on a Fast Train
+I Can't Turn You Loose
+I Didn't Know
+I Don't Care
+I Found a Reason
+I Get a Kick Out of You
+I Just Want To See His Face
+I Kissed a Girl
+I Know a Little
+I Shall Be Released
+I Told You So
+I Walk the Line
+I Wan'na Be Like You
+I Want To Be a Cowboy's Sweetheart
+I Will
+I Will Set You Free
+I Wish
+I'll Come Running
+I'm Blue, I'm Lonesome
+I'm Gonna Be (500 Miles)
+I'm One
+I'm So Tired
+I've Had Enough
+I've Turned Bad
+Icculus
+Idea
+If I Could
+If I Don't Be There By Morning
+If I Only Had a Brain
+If I Told You
+If You Need a Fool
+Immigrant Song
+In a Hole
+In a Mellow Tone
+In a Misty Glade
+In an Intensive Care Unit
+In the Aeroplane Over the Sea
+In The Good Old Summer Time
+In the Midnight Hour
+Infinite
+Ingest
+Insects
+Instant Karma!
+Intro
+Invisible
+Iron Man
+Is It In My Head?
+Is This What You Wanted
+It Ain't Easy
+It's All Right
+It's Ice
+Izabella
+JÃ¤germeister Song
+Jam
+Jennifer Dances
+Jessica
+Jesus Just Left Chicago
+John Hardy
+Johnny B. Goode
+Join the Band
+Joy
+Julia
+Julius
+Jump Monk
+Jumpin' Jack Flash
+Jungle Boogie
+Keeping It Real
+Keyboard Army
+Keyboard Cavalry
+Kill Devil Falls
+Killer Joe
+Killing in the Name
+Kung
+L.A. Woman
+La Grange
+Lady Stardust
+Last Victor Jam
+Lawn Boy
+Layla
+Lazy and Red
+Le Freak
+Leaves
+Legalize It
+Lengthwise
+Leprechaun
+Let It Loose
+Let Me Lie
+Let's Go
+Let's Go
+Letter to Jimmy Page
+Life on Mars?
+Lifeboy
+Light
+Light Up Or Leave Me Alone
+Limb By Limb
+Liquid Time
+Listening Wind
+Lit O Bit
+Little Red Rooster
+Little Tiny Butter Biscuits
+Lively Up Yourself
+Llama
+Lonesome Cowboy Bill
+Long Cool Woman in a Black Dress
+Long Long Long
+Look Out Cleveland
+Loup Garou
+Love Is What We Are
+Love Me
+Love Me Like a Man
+Love You
+Love, Reign O'er Me
+Loving Cup
+Low Rider
+Lucky Seven
+Lucy Had A Lumpy Head
+Lullaby of Birdland
+Lushington
+Maggie's Revenge
+Magilla
+Maiden Voyage
+Makisupa Policeman
+Mallory
+Manteca
+Maple Leaf Rag
+Marijuana Hot Chocolate
+Marissa
+Martha My Dear
+Martian Monster
+Maybe
+Maze
+McGrupp and the Watchful Hosemasters
+Mean Mr. Mustard
+Meat
+Meatstick
+Mellow Mood
+Melt
+Melt the Guns
+Memories
+Mercenary Territory
+Mercury
+Merry Pranksters Jam
+Messin' with The Kid
+Metal Bagel Death
+Mexican Cousin
+Middle of the Road
+Midnight Moonlight
+Midnight on the Highway
+Midnight Rider
+Mike's Song
+Mind Left Body Jam
+Minestrone
+Minkin
+Minute by Minute
+Mirror in the Bathroom
+Miss You
+Mist
+Misty Mountain Hop
+MMGAMOIO
+MmmBop
+Mo' Better Blues
+Moby Dick
+Mock Song
+Money
+Monkey Man
+Montana
+Moonage Daydream
+Moonlight in Vermont
+Moose the Mooche
+More
+Most Events Aren't Planned
+Mother Nature's Son
+Mound
+Mountains in the Mist
+Mozambique
+Mr. Completely
+Mr. P.C.
+Mustang Sally
+My Favorite Things
+My Friend, My Friend
+My Generation
+My Left Toe
+My Life As a Pez
+My Long Journey Home
+My Mind's Got a Mind of its Own
+My Problem Right There
+My Soul
+My Sweet One
+N2O
+Nellie Kane
+Never
+New Age
+New York, New York
+NICU
+Night and Day
+Night Moves
+Night Nurse
+Ninety-Nine Years (and One Dark Day)
+No Dogs Allowed
+No Good Trying
+No Men In No Man's Land
+No Quarter
+No Reply At All
+NO2
+Norwegian Wood (This Bird Has Flown)
+Not Fade Away
+Nothin' But A Nothin'
+Nothing
+Nowhere Fast
+O Canada
+O Holy Night
+O Mio Babbino Caro
+Ob-La-Di, Ob-La-Da
+Oblivious Fool
+Ocelot
+Oh Atlanta
+Oh! Sweet Nuthin'
+Old Folks Boogie
+Olivia's Pool
+On Green Dolphin Street
+On My Knees
+On the Road Again
+On The Run
+On Your Way Down
+Once in a Lifetime
+One Meatball
+Only a Dream
+Only Shallow
+Oye Como Va
+P-Funk Medley
+Party Time
+Passing Through
+Paul and Silas
+Peaches en Regalia
+Pebbles and Marbles
+Petrichor
+Phase Dance
+Piano Duet
+Pig in a Pen
+Piggies
+Pigtail
+Piper
+Plasma
+Play by Play
+Playing in the Band
+Poor Heart
+Possum
+Powderfinger
+Prep School Hippie
+Prince Caspian
+Proud Mary
+Psycho Killer
+Punch Me in the Eye
+Punch You in the Eye
+Purple Hugh
+Purple Rain
+Pusherman
+PYITE
+Quadrophenia
+Quadrophonic Toppling
+Quinn the Eskimo
+Ramble On
+Rapper's Delight
+Reba
+Rescue Squad
+Revival
+Revolution
+Revolution 1
+Revolution 9
+Revolution's Over
+Rhinoceros
+Rhombus Narration
+Rhymes
+Ride Captain Ride
+Rift
+Riker's Mailbox
+Rip This Joint
+Rise/Come Together
+Roadhouse Blues
+Roadrunner
+Rock 'n' Roll Suicide
+Rock A William
+Rock and Roll
+Rock and Roll All Nite
+Rock and Roll Part Two
+Rock Me Baby
+Rocket in My Pocket
+Rocket Man
+Rocketsled's Funky Main Man Meets Big Joe on Church Street
+Rocks Off
+Rocky Mountain Way
+Rocky Raccoon
+Rocky Top
+Roggae
+Roll in My Sweet Baby's Arms
+Roll Like a Cantaloupe
+Roses Are Free
+Rotation Jam
+Round Room
+Ruby Waves
+Run Like an Antelope
+Runaway Jim
+Runnin' with the Devil
+Running Out of Time
+Sabotage
+Sad Lisa
+Sailin' Shoes
+Sample in a Jar
+Samson and Delilah
+Samson Variation
+Sand
+Sanity
+Satin Doll
+Savoy Truffle
+Saw It Again
+Say It To Me S.A.N.T.O.S.
+Say Something
+Scabbard
+Scarlet Begonias
+Scent of a Mule
+Scents and Subtle Sounds
+Scents and Subtle Sounds (Intro)
+Sea and Sand
+Secret Language Instructions
+Secret Smile
+See That My Grave Is Kept Clean
+Seen and Not Seen
+Self
+Send in the Clowns
+Set Your Soul Free
+Setting Sail
+Seven Below
+Sexual Healing
+Sexy Sadie
+Shade
+Shafty
+Shaggy Dog
+Shake Your Coconuts
+Shake Your Hips
+She Caught the Katy and Left Me a Mule to Ride
+She Thinks I Still Care
+She's So Cold
+Shine
+Shine a Light
+Shipwreck
+Show of Life
+Shrine
+Sightless Escape
+Sigma Oasis
+Silent in the Morning
+Simple
+Sing Monica
+Sixteen Candles
+Skin It Back
+Skippy the Wondermouse
+Sky Train Wand
+Slave to the Traffic Light
+Sleep
+Sleep Again
+Sleeping Monkey
+Sleepwalk
+Slipknot!
+Smells Like Teen Spirit
+Smoke on the Water
+Sneakin' Sally Through the Alley
+Snow
+So Lonely
+So What
+SOAM
+Somantin
+Something
+Somewhere Over the Rainbow
+Soul Love
+Soul Planet
+Soul Shakedown Party
+Soul Survivor
+Space Oddity
+Spanish Flea
+Spanish Moon
+Sparkle
+Sparks
+Speak to Me
+Spices
+Splinters of Hail
+Split Open and Melt
+Spock's Brain
+Spooky
+Spread It 'Round
+Squeeze Box
+St. Stephen
+St. Thomas
+Stairway to Heaven
+Stand!
+Star
+Starman
+Stash
+Stealing Time From the Faulty Plan
+Steam
+Steep
+Steve Reich
+Stir It Up
+Stop Breaking Down
+Storage Jam
+Strange Design
+Strawberry Fields Forever
+Strawberry Letter 23
+Stray Dog
+Suffragette City
+Sugar Shack
+Summer of '89
+Sunday Morning
+Sunshine of Your Feeling
+Sunshine Superman
+Susie Q
+Suspicious Minds
+Susskind Hotel
+Suzy Greenberg
+Sweet Adeline
+Sweet Black Angel
+Sweet Emotion
+Sweet Georgia Brown
+Sweet Jane
+Sweet Virginia
+Swept Away
+Swing Low, Sweet Chariot
+Sympathy for the Devil
+T.V. Show
+T.V. Theme
+Take Me Out to the Ballgame
+Take Me to the River
+Take the 'A' Train
+Takin' Care of Business
+Talk
+Taste
+Taste That Surrounds
+Tela
+Tell Me Something Good
+Tennessee Waltz
+Terrapin
+Terrapin Station
+Thank You
+That's Alright Mama
+The Asse Festival
+The Ballad of Curtis Loew
+The Birds
+The Birdwatcher
+The Cataract Song
+The Chase
+The Chicken
+The Chinese Water Torture
+The Connection
+The Continuing Story of Bungalow Bill
+The Cover of 'Rolling Stone'
+The Curtain
+The Curtain With
+The Dickie Scotland Song
+The Dirty Jobs
+The Divided Sky
+The Dogs
+The Final Hurrah
+The Fishin' Hole
+The Fog That Surrounds
+The Gambler
+The Great Curve
+The Great Gig in the Sky
+The Happy Whip and Dung Song
+The Haunted House
+The Horse
+The Inlaw Josie Wales
+The Landlady
+The Last Step
+The Line
+The Lion Sleeps Tonight
+The Little Drummer Boy
+The Lizards
+The Maker
+The Man Who Stepped Into Yesterday
+The Mango Song
+The Moma Dance
+The Name is Slick
+The Ocean
+The Oh Kee Pa Ceremony
+The Old Home Place
+The Other One
+The Overload
+The Pendulum
+The Practical Song
+The Price of Love
+The Prison Joke
+The Punk Meets the Godfather
+The Real Me
+The Rock
+The Rover
+The Sloth
+The Squirming Coil
+The Star Spangled Banner
+The Tears of a Clown
+The Thrill is Gone
+The Unsafe Bridge
+The Very Long Fuse
+The Vibration of Life
+The Way It Goes
+The Wedge
+The Wind Cries Mary
+Them Changes
+Theme from Star Trek
+Theme from The Andy Griffith Show
+Theme From the Bottom
+Things People Do
+Thread
+Three Little Birds
+Thunder Road
+Thunderhead
+Tide Turns
+Timber
+Timber (Jerry The Mule)
+Time
+Time Loves a Hero
+Time Turns Elastic
+Tiny
+Title Track
+TMWSIY
+To France
+Tomorrow's Song
+Tonight
+Too Much of Everything
+Torn and Frayed
+Touch Me
+Tower Jam
+Train Round the Bend
+Train Song
+Treat Me Like a Fool
+Trench Town Rock
+Tripe Face Boogie
+Tropical Hot Dog Night
+Trust Fund Baby
+TTE
+Tube
+Tubthumping
+Tuesday
+Tuesday's Gone
+Tumbling Dice
+Turd on the Run
+Turtle in the Clouds
+Tush
+Tweezer
+Tweezer Reprise
+Twenty Years Later
+Twist
+Two Versions of Me
+Uncle Pen
+Uncloudy Day
+Undermind
+Undun
+Union Federal
+United We Stand
+Unknown
+Us and Them
+Vacuum Solo
+Ventilator Blues
+Victim
+Victor Jam Session
+Viola Lee Blues
+Voodoo Child (Slight Return)
+Vultures
+Wading in the Velvet Sea
+Wait
+Waiting All Night
+Waking Up
+Waking Up Dead
+Walfredo
+Walk Away
+Walk This Way
+Walls of the Cave
+Waste
+Watcher of the Skies
+Water in the Sky
+Waves
+Way Down in the Hole
+We Are Come to Outlive Our Brains
+We're an American Band
+We're Not Gonna Take It
+Weekapaug Groove
+Weekly Time
+Weigh
+West L.A. Fadeaway
+What Things Seem
+What You Will
+What's the Use?
+When Something is Wrong with My Baby
+When the Cactus is in Bloom
+When the Circus Comes
+While My Guitar Gently Weeps
+Whipping Post
+White Rabbit
+White Winter Hymnal
+Who By Fire
+Who Do? We Do!
+Who Knows
+Who Loves the Sun?
+Whole Lotta Love
+Why Don't We Do It in the Road?
+Why Don't You Love Me?
+Why You Been Gone So Long?
+Wider
+Wild Child
+Wild Honey Pie
+Wild Thing
+Wildwood Weed
+Will It Go Round in Circles
+Will the Circle Be Unbroken?
+Willin'
+Wilson
+Wind Beneath My Wings
+Windham Hell
+Windora Bug
+Windy City
+Wingsuit
+Winterqueen
+Wipe Out
+Wolfman's Brother
+Wombat
+Woodstock
+Wormtown
+Ya Mar
+Yarmouth Road
+YEM
+Yer Blues
+Yerushalayim Shel Zahav
+You Ain't Goin' Nowhere
+You Better Believe It Baby
+You Enjoy Myself
+You Gotta See Mama Every Night
+You Never Know
+You Sexy Thing
+You Shook Me All Night Long
+Your Pet Cat
+Ziggy Stardust`.split("\n");
